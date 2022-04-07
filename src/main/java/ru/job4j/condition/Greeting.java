36ada9f6bd7e3dc65fd6745d2aa7363d4f6b64ee
Kuzmich36rus <sledak-vrn@yandex.ru>
2022-04-07 23:01:02 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+import java.sql.Date;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String idea2 = idea + "But I am a newbie.";
+        int year = 2022;
+        String idea3 = idea2 + 2022;
+        System.out.println(idea3);
+
+    }
+}
